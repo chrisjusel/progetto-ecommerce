8a7eec0fcae98c3472c3b5c24486d7217dadda19
chrisjusel <christianvit@hotmail.it>
2022-04-15 17:33:22 +0200

    login e registrazione

--- client/js/script.js
+++ client/js/script.js
@@ -1,22 +1,28 @@
 const urlAPI="http://localhost:3000/api/users"
+
 let usersList = [];
+let loggedUser;
 
 document.addEventListener('DOMContentLoaded', function(){
     getAllUsers();
-    document.getElementById('submit').addEventListener('click', addUser);
-    document.getElementById('submit').addEventListener('click', getAllUsers);
+
+    if(document.location.pathname == '/client/register.html'){
+        document.getElementById('register').addEventListener('click', addUser);
+    }
+    else if(document.location.pathname == '/client/login.html'){
+        document.getElementById('logga').addEventListener('click', login);
+    }
 })
 
 function getAllUsers(){
     fetch(urlAPI).then(response => response.json()).then(json => {
         userList = json;
-        stampaUtenti();
     });
 }
 
 function addUser(){
-    let input = document.querySelectorAll('.container input');
-    let obj = {id: null, name: input[0].value, lastName: input[1].value, email: input[2].value, admin: false}
+    let input = document.querySelectorAll('#register input');
+    let obj = {id: null, name: input[0].value, lastName: input[1].value, email: input[2].value, password: input[3].value, admin: false}
     fetch(urlAPI,
         {
         method: 'POST',
@@ -26,5 +32,33 @@ function addUser(){
     input[0].value = '';
     input[1].value = '';
     input[2].value = '';
+    input[3].value = '';
     getAllUsers();
+}
+
+function login(e){
+    e.preventDefault();
+    let input = document.querySelectorAll('#login input');
+    let obj = {email: input[0].value, password: input[1].value};
+    fetch(urlAPI+'/login/',
+        {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {'Content-type': 'application/json'}
+    }).then(response => response.json()).then(json => {
+        if(json != null){
+            loggedUser = json;
+            let out = document.getElementById('outputLogin');
+            out.innerHTML = 'Accesso effettuato con successo';
+            out.style.color = 'green';
+        }
+        else{
+            let out = document.getElementById('outputLogin');
+            out.innerHTML = 'Password o email errati, riprova';
+            out.style.color = 'red';
+        }
+        console.log(loggedUser);
+    });
+    input[0].value = '';
+    input[1].value = '';
 }
--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 let users = 
 [
-    {id: 1, name: 'Admin', lastName: 'Administrator', email: 'admin@admin.it', admin: true}
+    {id: 1, name: 'Admin', lastName: 'Administrator', email: 'admin@admin.it', password: '123', admin: true}
 ];
+
+
 
 let count = users.length;
 
@@ -24,6 +26,24 @@ app.post('/api/users', (request, response) => {
     obj.id = ++count; //generazione automatica di un nuovo id univoco
     users.push(obj); //push nell'array di utenti dell'utente nuovo
     response.json('Utente aggiunto con successo'); //messaggio di successo
+})
+
+app.post('/api/users/login', (request, response) => {
+    let obj = request.body; //unico modo per passare i dati al backend è tramite una request
+    let email = obj.email;
+    let password = obj.password;
+    let loggedObj = users.find(ele => ele.email === email);
+    if(loggedObj != undefined){
+        if(loggedObj.password === password){
+            response.json(loggedObj);
+        }
+        else{
+            response.json(null);
+        }
+    }
+    else{
+        response.json(null);
+    }
 })
 
 app.listen(3000, () => console.log('Server attivo sulla porta 3000'));
