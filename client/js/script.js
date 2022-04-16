7a2db3eba9a410fb562cde09fad2a00986c87308
chrisjusel <christianvit@hotmail.it>
2022-04-16 15:51:23 +0200

    validazione form articoli e bottone cancella

--- client/js/script.js
+++ client/js/script.js
@@ -1,10 +1,10 @@
 const urlAPI="http://localhost:3000/api/users"
+const itemsAPI="http://localhost:3000/api/items"
 
-let usersList = [];
+let itemsList = [];
 let loggedUser;
 
 document.addEventListener('DOMContentLoaded', function(){
-    getAllUsers();
 
     if(document.location.pathname == '/client/register.html'){
         document.getElementById('register').addEventListener('click', addUser);
@@ -12,16 +12,22 @@ document.addEventListener('DOMContentLoaded', function(){
     else if(document.location.pathname == '/client/login.html'){
         document.getElementById('logga').addEventListener('click', login);
     }
+    else if(document.location.pathname == '/client/controlpanel.html'){
+        getAllItems();
+        document.getElementById('addItem').addEventListener('click', addItem);
+    }
 })
 
-function getAllUsers(){
-    fetch(urlAPI).then(response => response.json()).then(json => {
-        userList = json;
+function getAllItems(){
+    fetch(itemsAPI).then(response => response.json()).then(json => {
+        itemsList = json;
+        printItemList();
     });
 }
 
-function addUser(){
-    let input = document.querySelectorAll('#register input');
+function addUser(e){
+    e.preventDefault();
+    let input = document.querySelectorAll('#registrazione input');
     let obj = {id: null, name: input[0].value, lastName: input[1].value, email: input[2].value, password: input[3].value, admin: false}
     fetch(urlAPI,
         {
@@ -33,8 +39,42 @@ function addUser(){
     input[1].value = '';
     input[2].value = '';
     input[3].value = '';
-    getAllUsers();
 }
+
+function addItem(e){
+    e.preventDefault();
+    let input = document.querySelectorAll('#aggiungiProdotti input');
+    let obj = {id: null, name: input[0].value, price: input[1].value, description: input[2].value, image: input[3].value}
+    let out = document.getElementById('addItemError');
+
+    if(validaItem(obj)){
+        fetch(itemsAPI,
+            {
+            method: 'POST',
+            body: JSON.stringify(obj),
+            headers: {'Content-type': 'application/json'}
+            }).then(response => response.json()).then(json => {
+                console.log(json)
+                getAllItems();
+            });
+        input[0].value = '';
+        input[1].value = '';
+        input[2].value = '';
+        input[3].value = '';
+        out.innerHTML = '';
+    } else {
+        out.innerHTML = 'Dati inseriti non validi, riprova';
+        out.style.color = 'red';
+    }
+}
+
+function removeItem(id){
+    fetch(itemsAPI+'/'+id, {method: 'DELETE'}).then(response => response.json()).then(json => { 
+        console.log(json);
+        getAllItems();
+    })
+}
+
 
 function login(e){
     e.preventDefault();
@@ -61,4 +101,45 @@ function login(e){
     });
     input[0].value = '';
     input[1].value = '';
+}
+
+function printItemList(){
+    let lista = document.querySelector('#listaProdotti');
+    lista.innerHTML = '';
+    itemsList.forEach(ele => {
+        let tr = document.createElement('tr');
+        let deleteButton = document.createElement('button');
+        deleteButton.className = 'deleteButton btn btn-danger';
+        deleteButton.innerText = 'X';
+        deleteButton.addEventListener('click', () => removeItem(ele.id));
+        tr.innerHTML = 
+        `
+        <td>${ele.id}</td>
+        <td>${ele.name}</td>
+        <td>${ele.description}</td>
+        <td>${ele.price}€</td>
+        <td><img src="${ele.image}"></td>
+        ` //NELL'IMMAGINE DEVO CONTROLLARE SE C'E' L'HTTPS:// SENNO' DA ERRORE
+        tr.appendChild(deleteButton);
+        lista.appendChild(tr);
+    })
+
+}
+
+function validaItem(obj){
+    let nome = obj.name;
+    let prezzo = obj.price;
+    let description = obj.description;
+    let image = obj.image;
+
+    if(nome != '' && !isNaN(prezzo) && 
+    prezzo != '' && description != '' && 
+    (image.substring(0, 8) === 'https://' || image.substring(0, 7) === 'http://')){
+        console.log(true);
+        return true;
+    }
+    else{
+        console.log(false);
+        return false;
+    }
 }
--- server/index.js
+++ server/index.js
@@ -16,10 +16,11 @@ let users =
 let items = 
 [
     {id: 1, name: 'Scape', price: 50, description: 'Le scarpe più belle di Napoli', image: 'https://m.media-amazon.com/images/I/61yZW44DxoL._AC_UX500_.jpg'},
-    {id: 2, name: 'Pantalone', price: 30, description: 'Il pantalone più bello di Napoli', image: 'https://m.media-amazon.com/images/I/61yZW44DxoL._AC_UX500_.jpg'}
+    {id: 2, name: 'Pantalone', price: 30, description: 'Il pantalone più bello di Napoli', image: 'https://www.ivgoutlet.it/2252-medium_default/tuta-acetata-ssc-napoli-303jpi0-910-azzurra.jpg'}
 ]
 
 let count = users.length;
+let countItems = items.length;
 
 app.get('/api/users', (request, response) => {
     response.json(users);
@@ -34,6 +35,19 @@ app.post('/api/users', (request, response) => {
     obj.id = ++count; //generazione automatica di un nuovo id univoco
     users.push(obj); //push nell'array di utenti dell'utente nuovo
     response.json('Utente aggiunto con successo'); //messaggio di successo
+})
+
+app.post('/api/items', (request, response) => {
+    let obj = request.body;
+    obj.id = ++countItems;
+    items.push(obj);
+    response.json('Item aggiunto con successo');
+})
+
+app.delete('/api/items/:id', (request, response) => {
+    const id = request.params.id;
+    items = items.filter(item => item.id !== +id); //mi creo un array con tutti gli elementi tranne quello che voglio eliminare
+    response.json('Item eliminato con successo');
 })
 
 app.post('/api/users/login', (request, response) => {
@@ -53,5 +67,6 @@ app.post('/api/users/login', (request, response) => {
         response.json(null);
     }
 })
+
 
 app.listen(3000, () => console.log('Server attivo sulla porta 3000'));
